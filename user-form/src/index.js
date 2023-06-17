4646b4bb1d8e9503b4b807a1905adb7bd10f4106
Jayadeep JK <jayadeep@zeliot.in>
2023-06-17 18:49:48 +0530

    browser router removed

--- user-form/src/index.js
+++ user-form/src/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
-import App from "./App";
 import theme from "./styles/theme";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
   document.getElementById("root")
 );
