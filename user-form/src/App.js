e2e9507cc302e64cd3c56ebba921d38637402d29
Jayadeep JK <jayadeep@zeliot.in>
2023-06-17 18:49:22 +0530

    AppRoutes imported

--- user-form/src/App.js
+++ user-form/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { ThemeProvider } from "@mui/material/styles";
-import Routes from "./routes";
-
 import theme from "./styles/theme";
+import AppRoutes from "./routes";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Routes />
+      <AppRoutes />
     </ThemeProvider>
   );
 };
